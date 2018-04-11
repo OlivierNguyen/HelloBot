8bc3f59d63accfae3b5809ac29e013bca9f972e5
OlivierNguyen <olivier_nguyen@live.fr>
2018-04-11 13:48:04 +0200

    Update settings.js
    change recast api key

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 export const SETTINGS = {
     API_ROOT: 'https://api.recast.ai',
-    RECAST_API_KEY: 'ca87c30ddc9259b10bcf0e70dcc5dbf0', // KEY TO CHANGE
+    RECAST_API_KEY: '5959b9ec5b29700e314367027d3de353', // KEY TO CHANGE
     BOT_LANGUAGE: 'fr'
 };
